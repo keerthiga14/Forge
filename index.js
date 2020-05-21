634c0aee081c7d02bb3a4a904d4b2a9c6ff92b8e
keerthiga14 <keerthiga.17ec@kct.ac.in>
2020-05-21 12:57:07 +0530

    ignore

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 // var faker = require('faker')
 
-mongoose.connect("mongodb+srv://admin:<password>@cluster0-tntjf.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true})
-.then(()=>{console.log("db connected")}).catch((err)=>{console.log(err)})
+mongoose.connect("mongodb+srv://admin:admin@cluster0-cwqar.mongodb.net/test?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{console.log("db connected")}).catch((err)=>{console.log(err)})
+
+const dataSchema = new mongoose.Schema({
+    name:"String",
+    team:"String"
+})
+
+var User = mongoose.model("User",dataSchema)
 
 app.use(bodyParser.urlencoded(
     { extended: true }
@@ -13,18 +19,34 @@ app.use(bodyParser.urlencoded(
 
 app.use(express.static('views'))
 
-app.get('/',(req,res)=>{
-    // res.sendFile('index.ejs')
-    // var route = req.params.id
-    // var yrname = 'strap'
-    // var fakeName = faker.name.findName
-    // var fakeJob = faker.name.jobTitle
-    // var fakeJobDesc = faker.name.jobDescriptor
-    res.render("index.ejs")
+// app.get('/',(req,res)=>{
+//     // res.sendFile('index.ejs')
+//     // var route = req.params.id
+//     // var yrname = 'strap'
+//     // var fakeName = faker.name.findName
+//     // var fakeJob = faker.name.jobTitle
+//     // var fakeJobDesc = faker.name.jobDescriptor
+//     res.render("index.ejs")
+// })
+app.get('/', (req,res)=>
+{
+   User.find({},(err,data)=>{
+       if(err){
+           console.log('Cannot find data',err)
+       }
+       else{
+           res.render('index.ejs',{user:data})
+       }
+   })
+    // res.render('index.ejs')
 })
 
 app.post('/test',(req,res)=>{
-    console.log(req.body)
+    // console.log(req.body)
+    var newUser = new User({
+        name:req.body.name,
+        team:req.body.team
+    }).save().then(savedData => console.log("data saved",savedData)).catch(err => console.log)
     res.redirect('/')
 })
 
